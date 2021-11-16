de50f57756a054607b581882fc0ecd50d83b6ebc
Elizabeth3918 <liza14lab@gmail.com>
2021-11-16 13:09:02 +0200

    View_add

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
 #include <iostream>
-#include "Game.h"
-#include "stdafx.h"
 #include <cmath>
 using namespace std;
 int w = 600, h= 600;
 float a = -10, b = 10;
 float c = -10, d = 10;
-float t = 0, dt = 0.001;
+float t = 0, dt = 0.0001;
+float alp=0, dalp=0.01;
 sf::RenderWindow window(sf::VideoMode(w, h), "Func graph", sf::Style::Default);
 sf::View view;
+sf::View pixelView;
+sf::Font font;
+sf::Text text;
 float transform_x(float x)
 {
-    return (x-a)/(b-a)*w;
+    return x;
+    //return (x-a)/(b-a)*w;
 }
 float transform_y(float y)
 {
-    return (y-d)/(c-d)*h;
+    return y;
+    //return (y-d)/(c-d)*h;
 }
 
 void drawLine(float x1, float y1, float x2, float y2, sf::Color color = sf::Color::White){
@@ -29,15 +34,21 @@ void drawLine(float x1, float y1, float x2, float y2, sf::Color color = sf::Colo
     window.draw(line, 2, sf::Lines);
 }
 float f(float x){
-    //return logf(powf(5*M_PI*(x+9), sinf(5*(x+9)))) * 0.5 * asinf(cosf(5*M_PI*(x+2*sinf(t/2))-M_PI/2));
+    return logf(powf(5*M_PI*(x+23), sinf(5*(x+3)))) * 0.5 * asinf(cosf(5*M_PI*(x+2*sinf(t/2))-M_PI/2));
     //return t*sin(x*t);
-    return cos(x/3)*sin(x*t) + t;
+    //return cos(x/3)*sin(x*t) + t;
 }
-float OX(float x){
-    return x = 0;
-}
-float OY(float y){
-    return y = 0;
+
+void move()
+{
+    if (t > 5 || t < -5) {
+        dt = -dt;
+    }
+    t += dt;
+    alp += dalp;
+    view.setRotation(alp);
+    pixelView.setRotation(alp);
+
 }
 void drawdash(){
     drawLine(a, 0, b, 0, sf::Color::Red);
@@ -46,18 +57,29 @@ void drawdash(){
     float dash_len = 3.;
     while(dash <= b){
         drawLine(dash, -dash_len*(c-d)/h, dash, dash_len*(c-d)/h, sf::Color::White);
+        window.setView(pixelView);
+        text.setString(to_string((int)t));
+        text.setPosition((t-a)*w/(b-a)-6, h/2 + 6);
+        window.draw(text);
+        window.setView(view);
         dash += 1;
     }
     dash = (int)c + 1;
     while(dash<=d)
     {
         drawLine(-dash_len*(b-a)/w, dash, dash_len*(d-c)/w,dash,  sf::Color::White);
+        window.setView(pixelView);
+        text.setString(to_string((int)t));
+        text.setPosition(w/2 + 6, (t-d)/(c-d)*h -9);
+        window.draw(text);
+        window.setView(view);
         dash += 1;
     }
 }
 void draw(){
     float x=a, prev_x;
     float dx=0.1;
+    window.setView(view);
     drawdash();
     //[a, b] -> [0, w]
     //a-> 0, b -> w => x -> x' => (x-a)/(b-a) * w
@@ -73,16 +95,16 @@ void draw(){
 
     }
 }
-void move()
-{
-    if (t > 5 || t < -5) {
-        dt = -dt;
-    }
-    t += dt;
-}
 
 int main(){
     srand(time(0));
+    /*if (!font.loadFromFile('OpenSans-ExtraBold.ttf')){
+        return -1;
+    }*/
+    text.setFont(font);
+    text.setCharacterSize(12);
+    text.setFillColor(sf::Color::Red);
+    pixelView.reset(sf::FloatRect(0, 0, w, h));
     view.reset(sf::FloatRect(a, c, b-a, d-c));
     while(window.isOpen())
     {
@@ -92,35 +114,9 @@ int main(){
             if(event.type == sf::Event::Closed)
                 window.close();
         }
-        //check for movement keys pressed
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-        {
-            if(!(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S)))
-            {
-                //move up
-            }
-        }
-        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-        {
-            //move down
-            view.move(1.0f, 0.0f);
-        }
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-        {
-            if(!(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D)))
-            {
-                //move left
-            }
-        }
-        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-        {
-            //right
-            view.move(0.0, 1.0);
-        }
+        move();
         window.clear();
         draw();
-        move();
         window.display();
     }
     return 0;
